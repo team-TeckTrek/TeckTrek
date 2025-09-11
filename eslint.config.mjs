fe159ab2f73e72e145f22c8fb495b29027e5235f
今城　颯 <sajincheng6@gmail.com>
2025-09-11 22:22:47 +0900

    #40Storybook環境構築

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
@@ -10,27 +13,24 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'prettier'),
-  {
-    plugins: {
-      'unused-imports': unusedImports,
-    },
-    rules: {
-      'no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'error',
-      semi: ['error', 'never'],
-      'unused-imports/no-unused-vars': [
-        'error',
-        {
-          vars: 'all',
-          varsIgnorePattern: '^_',
-          args: 'after-used',
-          argsIgnorePattern: '^_',
-        },
-      ],
-    },
+const eslintConfig = [...compat.extends('next/core-web-vitals', 'prettier'), {
+  plugins: {
+    'unused-imports': unusedImports,
   },
-]
+  rules: {
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    semi: ['error', 'never'],
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+}, ...storybook.configs["flat/recommended"]]
 
 export default eslintConfig
