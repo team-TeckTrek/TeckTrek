4f698191fb305c22d4d681a3ac47fcf109d4e1f4
sekiyasorato <songan0226@gmail.com>
2025-09-11 21:50:23 +0900

    #29　トップ画面UI実装

---
+++ src/app/top/page.tsx
@@ -0,0 +1,11 @@
+export default function TopPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-[repeating-linear-gradient(90deg,#ffd9bf,#ffd9bf_40px,#ffe6d6_40px,#ffe6d6_80px)]">
+      <h1 className="absolute top-1/7 left-1/2 -translate-x-1/2 text-[5vw] font-normal font-bold text-[#352107] text-center leading-relaxed">
+        Teck Trek
+        <br />
+        絵しりとり
+      </h1>
+    </main>
+  )
+}
