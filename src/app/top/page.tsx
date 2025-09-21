492613da13c95cf4870d3531c1516c7258265e31
sekiyasorato <songan0226@gmail.com>
2025-09-21 13:14:22 +0900

    feat: トップページ背景画像を追加

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -1,10 +1,19 @@
+import Image from 'next/image'
+
 export default function TopPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-[repeating-linear-gradient(90deg,#ffd9bf,#ffd9bf_40px,#ffe6d6_40px,#ffe6d6_80px)]">
-      <h1 className="absolute top-1/7 left-1/2 -translate-x-1/2 text-[5vw] font-normal font-bold text-[#352107] text-center leading-relaxed">
-        Teck Trek
-        <br />
-        絵しりとり
+    <main className="h-screen flex items-center justify-center relative">
+      <Image
+        src="/image/back-image.png"
+        alt="背景のストライプ"
+        fill
+        priority
+        sizes="100vw"
+        className="object-cover -z-10"
+      />
+      <h1 className="absolute top-1/7 left-1/2 -translate-x-1/2 text-[5vw] font-bold text-[#352107] text-center leading-relaxed">
+        <span className="block">Teck Trek</span>
+        <span className="block">絵しりとり</span>
       </h1>
     </main>
   )
