ed1efa56e21cb64e7f25a0015f19cc0711c8b795
DKen-DevCat <dken.devdev@gmail.com>
2025-09-22 22:08:58 +0900

    pageサンプルを作成

---
+++ src/app/play/page.tsx
@@ -0,0 +1,30 @@
+import { Sample } from '@/components/sample/sample'
+
+export default function Page() {
+  return (
+    <div className="h-screen flex flex-row gap-2 w-full py-[8px] px-[16px]">
+      <div className="flex flex-col gap-2 flex-[453] min-w-[453px]">
+        <div className="bg-neutral-200 flex-1 flex">
+          <Sample title="過去絵セクション" />
+        </div>
+        <div className="bg-neutral-200 flex-1 flex">
+          <Sample title="キャンバス" />
+        </div>
+        <div className="bg-neutral-200 flex-1 flex">
+          <Sample title="下のボタン群" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 flex-[183] min-w-[183px]">
+        <div className="bg-neutral-200 flex-1 flex">
+          <Sample title="タイム、退室" />
+        </div>
+        <div className="bg-neutral-200 flex-1 flex">
+          <Sample title="プレイヤーアイコン" />
+        </div>
+        <div className="bg-neutral-200 flex-1 flex">
+          <Sample title="回答するボタン、音楽" />
+        </div>
+      </div>
+    </div>
+  )
+}
