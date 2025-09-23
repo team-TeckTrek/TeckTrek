f4c19adad96e914502c2553a208fb02754afcaac
sekiyasorato <songan0226@gmail.com>
2025-09-23 16:14:16 +0900

    #feature/#29_top-page-ui

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -2,19 +2,21 @@ import Image from 'next/image'
 
 export default function TopPage() {
   return (
-    <main className="h-screen flex items-center justify-center relative">
+    <main className="h-screen relative flex flex-col items-center justify-center">
       <Image
         src="/image/back-image.png"
         alt="背景のストライプ"
         fill
         priority
-        sizes="100vw"
+        sizes="1920px"
         className="object-cover -z-10"
       />
-      <h1 className="absolute top-1/7 left-1/2 -translate-x-1/2 text-[5vw] font-bold text-[#352107] text-center leading-relaxed">
-        <span className="block">Teck Trek</span>
-        <span className="block">絵しりとり</span>
-      </h1>
+      <div className="flex flex-col items-center gap-2 -translate-y-[20vh]">
+        <h1 className="text-[72px] font-bold text-[#352107] text-center leading-relaxed">
+          <span className="block">Teck Trek</span>
+          <span className="block">絵しりとり</span>
+        </h1>
+      </div>
     </main>
   )
 }
