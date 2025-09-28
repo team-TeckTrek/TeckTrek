07e80bce33f1b3e9d1d9062d38af773f22e569e3
西平勇心 <nishihirayuushin@nishihirayuushinnoMacBook-Air.local>
2025-09-29 00:46:30 +0900

    fix

--- src/components/notification/CurrentDate.tsx
+++ src/components/notification/CurrentDate.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 const CurrentDate: React.FC = () => {
   const today = new Date();
   const formattedDate = today.toLocaleDateString();
-  console.log('取得した日付:', formattedDate);
 
   return <div>今日の日付: {formattedDate}</div>;
 };
