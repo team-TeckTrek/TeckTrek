9af5c42f1c4db0c5e0e20f2dabdea402cd39f432
西平勇心 <nishihirayuushin@nishihirayuushinnoMacBook-Air.local>
2026-01-19 23:03:31 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import NoticeList from '@/components/notification/NoticeList'
-import { fetchNotices } from '@/feature/notice/request'
-import { isError } from '@/feature/fetcher/errors'
+import { DEFAULT_NOTICES } from '@/constants/notices'
 
-export default async function Page() {
-  const response = await fetchNotices()
-  const notices = isError(response) ? [] : response.notices
-
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-gray-50">
       <div className="mt-8 w-[600px]">
-        <NoticeList notices={notices} />
+        <NoticeList notices={DEFAULT_NOTICES} />
       </div>
       {/* 他のコンテンツ */}
     </main>
